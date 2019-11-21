aed93b17b49e83e65bd32205c8b1979945829ed4
Armando074 <58048728+Armando074@users.noreply.github.com>
2019-11-21 16:02:40 -0600

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -12,8 +12,31 @@ http.createServer(function (req, res) {
     if (typeof jugador.nombre === 'string' && 
        typeof jugador.pos === 'object' &&
        typeof jugador.pos.length === 'number' &&
-       jugador.pos.length === 2){
-    res.end('correcto');
+       jugador.pos.length === 2 && 
+       typeof jugador.tam === 'number'){
+      // buscar obj
+      var indice = -1;
+      for (var i = 0; i < jugadores. length; i++){
+      if (jugadores[i].nom === jugador.nom;){
+      indice = i;
+        break;
+      }
+     }
+    if (indice < 0){
+      // crear
+      jugadores.push(jugador);
+      }else{
+      //actualizar
+        jugadores[indice]=jugador;
+        for (var i = 0; i< jugadores.length; i++){
+        var dx = jugadores[i].pos[0] - jugador.pos[0];
+        var dy = jugadores[i].pos[1] - jugador.pos[1];
+          
+          var d Marh.sqrt(dx * dx + dy * dy);
+        }
+        res.end('jugador actualizado')
+      }
+      
     }else{
     res.end('incorrecto');
     }
