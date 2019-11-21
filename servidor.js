ec60c959035e1918987dcaa61389321242cb10a4
Armando074 <58048728+Armando074@users.noreply.github.com>
2019-11-21 15:02:39 -0600

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var url = require('url');
 
 http.createServer(function (req, res) {
+  var q = url.parse(req.url,true).query;
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('saludos, este es un servidor');
+  res.end('HI' + q.nombre);
 }).listen(process.env.PORT);
