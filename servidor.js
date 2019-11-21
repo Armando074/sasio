057c580b00a973597f5cdba9eaf401855a620f0f
Armando074 <58048728+Armando074@users.noreply.github.com>
2019-11-21 15:20:12 -0600

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,11 +1,15 @@
 var http = require('http');
 var url = require('url');
 
-var nombres = [];
+var jugadores = [];
 
 http.createServer(function (req, res) {
-  var q = url.parse(req.url,true).query;
-  nombres.push(q.nombre);
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('lista de nombres' + nombres.join());
+  var q = url.parse(req.url,true).query;
+  if(q.jugador){
+    res.end('recibido')
+  // recibimos
+  } else {
+  res.end('Error');
+  }
 }).listen(process.env.PORT);
